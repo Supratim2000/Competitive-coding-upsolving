b0e1a3e6a58731292e4e54dd9c277ff36048f4aa
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2022-02-25 16:03:26 +0530

    Update C.cpp

--- Codeforces Round #773 (Div. 2)/C.cpp
+++ Codeforces Round #773 (Div. 2)/C.cpp
@@ -26,7 +26,7 @@ typedef unordered_map<ll,bool> umllb;
 #define pb push_back
 #define mp make_pair
 const ll mod=10e9+7;
-
+ 
 class Triplet
 {
     public:
@@ -40,7 +40,7 @@ class Triplet
         this->y=y;
     }
 };
-
+ 
 class Sieve
 {
     private:
@@ -55,11 +55,11 @@ class Sieve
                     for(ll j=i*i;j<=n;j+=i)
                         Prime[j]=false;
         }
-
+ 
     public:
         ll n;
         vector<bool> Prime;
-
+ 
         void printPrime()
         {
             for(ll i=0;i<=n;i++)
@@ -67,7 +67,7 @@ class Sieve
                     cout<<i<<" ";
             cout<<endl;
         }
-
+ 
         ll countPrime(ll l,ll h)
         {
             ll count=0;
@@ -86,16 +86,16 @@ class Sieve
             }
             return false;
         }
-
+ 
         Sieve(ll n)
         {
             this->n=n;
             genPrime();
         }
 };
-
+ 
 ll gcd(ll a,ll b) {return a==0?b:gcd(b % a, a);}
-
+ 
 Triplet gcdExt(ll a,ll b)
 {
     if(b==0)
@@ -103,7 +103,7 @@ Triplet gcdExt(ll a,ll b)
     Triplet smallRes=gcdExt(b,a%b);
     return Triplet(smallRes.gcd,smallRes.y,smallRes.x-(a/b)*smallRes.y);
 }
-
+ 
 ll multiModInvrs(ll a,ll m)
 {
     //if there exist any b (1<=b<=m-1) for which (a.b)modm=1 then b is multiplicative modulo inverse of a and m
@@ -120,7 +120,7 @@ ll multiModInvrs(ll a,ll m)
     else
         return -1;
 }
-
+ 
 bool isSorted(vll arr,int n)
 {
     if(n==1 || n==0)
@@ -132,12 +132,12 @@ bool isSorted(vll arr,int n)
     }
     return true;
 }
-
+ 
 bool isPresent(unordered_map<ll,ll>& mp, ll key)
 {
     return mp.find(key)!=mp.end();
 }
-
+ 
 ll solve()
 {
     ll n,x,count=0;
@@ -156,8 +156,6 @@ ll solve()
         {
             if(arr[i]%x==0 && isPresent(freq, arr[i]/x) && freq[arr[i]/x])
                 freq[arr[i]/x]--;
-            else if(isPresent(freq, arr[i]*x) && freq[arr[i]*x])
-                freq[arr[i]*x]--;
             else
                 count++;
             freq[arr[i]]--;
@@ -165,7 +163,7 @@ ll solve()
     }
     return count;
 }
-
+ 
 int main()
 {
     ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
@@ -180,6 +178,6 @@ int main()
         //solve();
         cout<<solve()<<endl;
     }
-
+ 
     return 0;
 }
